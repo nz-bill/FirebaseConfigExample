2735c0c77e9f9fe7635e9fd27f0f5ef4d378c3c7
nz-bill <bill.palmestedt@zocom.se>
2025-05-07 12:10:49 +0200

    added firebase config

--- FirebaseConfigExample/FirebaseConfigExampleApp.swift
+++ FirebaseConfigExample/FirebaseConfigExampleApp.swift
@@ -6,10 +6,52 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+      configureFirebase()
+    return true
+  }
+    
+    func configureFirebase() {
+        
+        // gör om vår secrets.plist till en dictionary för att enkelt komma åt datan
+        guard let path = Bundle.main.path(forResource: "Secrets", ofType: "plist"),
+              let dict = NSDictionary(contentsOfFile: path) as? [String: Any] else {
+            fatalError("Kunde inte läsa Secrets.plist")
+        }
+        
+        // vi skapar ett FirebaseOptionsObjekt.
+        //denna kan användas ist för GoogleService-Info.plist när vi sen konfigurerar firebase
+        // vi sätter värdena från vår nyligen skapade dictionary
+        let options = FirebaseOptions(
+            googleAppID: dict["APP_ID"] as! String,
+            gcmSenderID: dict["GCM_SENDER_ID"] as! String
+        )
+        options.apiKey = dict["API_KEY"] as? String
+        options.projectID = dict["PROJECT_ID"] as? String
+        options.storageBucket = dict["STORAGE_BUCKET"] as? String
+//        eventuellt fler fält. om du exempelvis använder google sign-in, lägger du till:
+//        options.clientID = dict["CLIENT_ID"] as? String
+
+
+        //nu kan vi skicka in våra options när vi anropar .configure
+        FirebaseApp.configure(options: options)
+    }
+}
 
 @main
 struct FirebaseConfigExampleApp: App {
     let persistenceController = PersistenceController.shared
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+//    init() {
+//            configureFirebase()
+//        }
 
     var body: some Scene {
         WindowGroup {
@@ -17,4 +59,6 @@ struct FirebaseConfigExampleApp: App {
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
+    
+    
 }
